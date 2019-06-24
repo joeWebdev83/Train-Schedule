3e7e3a9f9f4835d6e9ab8044e8ffeae9dd600078
joel petion <joelpetion@yahoo.com>
2019-06-24 08:38:44 -0400

    added html

---
+++ assets/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var firebaseConfig = {
+    apiKey: "AIzaSyAPZEIkfWoGHMka-_Gifkg9Czn5oQZpOog",
+    authDomain: "train-82753.firebaseapp.com",
+    databaseURL: "https://train-82753.firebaseio.com",
+    projectId: "train-82753",
+    storageBucket: "",
+    messagingSenderId: "587728696635",
+    appId: "1:587728696635:web:3ddc1cbf11ab9a25"
+  };
